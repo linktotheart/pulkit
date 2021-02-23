0324a12ce472dd7ff944e2021a0c71f42e762556
Pulkit Singh <44489787+pulkitsingh69@users.noreply.github.com>
2021-02-24 01:16:56 +0530

    newAbout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
-const siteMetadata = require('./site-metadata.json')
+const siteMetadata = require("./site-metadata.json");
 // new comment for dev /
 module.exports = {
-    pathPrefix: '/',
-    siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-source-data`,
-        `gatsby-transformer-remark`,
-        `gatsby-transformer-sharp`, 
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `pages`,
-                path: `${__dirname}/src/pages`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {}
-        },
-        {
-            resolve: `gatsby-remark-page-creator`,
-            options: {}
-        },
-        {
-            resolve: `@stackbit/gatsby-plugin-menus`,
-            options: {
-                sourceUrlPath: `fields.url`,
-                pageContextProperty: `menus`,
-            }
-        }
-    ]
+  pathPrefix: "/",
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-source-data`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-remark-page-creator`,
+      options: {},
+    },
+    {
+      resolve: `@stackbit/gatsby-plugin-menus`,
+      options: {
+        sourceUrlPath: `fields.url`,
+        pageContextProperty: `menus`,
+      },
+    },
+  ],
 };
